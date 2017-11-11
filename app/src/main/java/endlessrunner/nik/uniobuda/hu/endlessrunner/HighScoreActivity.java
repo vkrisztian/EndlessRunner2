b0fdf298ef24c15711688d8c155624409abefed0
Krisztián Vörös <v.vori@intellihome.hu>
2017-11-11 19:34:43 +0100

    new game es highscore activity

---
+++ app/src/main/java/endlessrunner/nik/uniobuda/hu/endlessrunner/HighScoreActivity.java
@@ -0,0 +1,13 @@
+package endlessrunner.nik.uniobuda.hu.endlessrunner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HighScoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_high_score_activitz);
+    }
+}
--- app/src/main/java/endlessrunner/nik/uniobuda/hu/endlessrunner/MainActivity.java
+++ app/src/main/java/endlessrunner/nik/uniobuda/hu/endlessrunner/MainActivity.java
@@ -1,17 +1,50 @@
 package endlessrunner.nik.uniobuda.hu.endlessrunner;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    private String username;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+    }
+
+    public void StartGame(View view) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Name: ")
+                .setTitle("New Game");
+        builder.setView(R.layout.dialog);
+        final AlertDialog dialog1 = builder.create();
+
+        dialog1.show();
+        Button OKButton = (Button) dialog1.findViewById(R.id.okbutton);
+        OKButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText et = (EditText) dialog1.findViewById(R.id.userName);
+                username = et.getText().toString();
+                dialog1.dismiss();
+            }
+        });
+
+
+    }
+
+    public void HighScore(View view) {
+        Intent hs = new Intent(this,HighScoreActivity.class);
+        startActivity(hs);
     }
 }
